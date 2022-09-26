8eb5b251328d53024ab46139e43862a6b38077d9
Mohanad Elamin <melamin@Mohanads-MacBook-Pro.local>
2022-09-26 15:04:36 +0200

    update typo

--- rke2-master.sh
+++ rke2-master.sh
@@ -6,7 +6,7 @@ curl -sfL https://get.rke2.io | sh -
 # Enable the rke2-server service
 systemctl enable rke2-server.service
 
-echo "Starting RKE2 service, you can run \"journalctl -u rke2-master -f\" on another terminal to follow the logs"
+echo "Starting RKE2 service, you can run \"journalctl -u rke2-server -f\" on another terminal to follow the logs"
 
 # Start the service
 systemctl start rke2-server.service
